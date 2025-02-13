5f0ab84fb66575628833397d280fa0cca2754e93
MrCooksIT <acoetzee@maristsj.co.za>
2025-02-13 14:01:38 +0200

    Add Firebase configuration

--- src/components/config/firebase.js
+++ src/components/config/firebase.js
@@ -1,18 +1,17 @@
-// src/config/firebase.js
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCdCifTm6-IU3ERw2S_AQ4_tZDf6FmdyCI",
-    authDomain: "campuscare-9b9d2.firebaseapp.com",
-    projectId: "campuscare-9b9d2",
-    storageBucket: "campuscare-9b9d2.firebasestorage.app",
-    messagingSenderId: "813279438668",
-    appId: "1:813279438668:web:0230e890f6b85dbc99d1e9",
-    measurementId: "G-N2501ZJ1QS"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
